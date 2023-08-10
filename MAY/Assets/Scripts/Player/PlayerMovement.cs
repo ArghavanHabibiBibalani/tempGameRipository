53c016fc3228e49eefd39c644ca0f880baa1d124
YB912 <YB912TU@gmail.com>
2023-08-10 20:38:00 +0430

    Setup input system and update player movement

--- MAY/Assets/Scripts/Player/PlayerMovement.cs
+++ MAY/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,59 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float speedController = 20f;
-    [SerializeField] float jumpSpeed = 10f;
-    [SerializeField] int jumpTimes = 2;
-    [SerializeField] LayerMask groroundLayer;
-    [SerializeField] Rigidbody2D rigidBody;
-    [SerializeField] Transform feet;
-    int jumpCounter = 0;
-    bool isGrounded;
-    float horizontalMove;
-    float jumpCoolDown;
+    [SerializeField] float horizontalMovementSpeed = 200f;
+    [SerializeField] float jumpSpeed = 30f;
+    [SerializeField] int jumpTimes;
+    [SerializeField] float groundedBoxCastOffset;
+    [SerializeField] PlayerInputActions playerInputActions;
+    [SerializeField] LayerMask groundLayerMask;
+    private float horizontalMovement;
+    private int jumpCounter;
+    private Rigidbody2D rigidBody;
+    private new Collider2D collider;
+    private void Awake()
+    {
+        jumpCounter = 0;
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerInputActions = new PlayerInputActions();
+        collider = GetComponent<Collider2D>();
+
+        playerInputActions.OnFoot.Enable();
+        playerInputActions.OnFoot.Jump.performed += OnJump;
+        playerInputActions.OnFoot.Move.performed += OnMove;
+        playerInputActions.OnFoot.Move.canceled += OnMove;
+    }
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * speedController * Time.deltaTime;//A == -1 == BACK AND D == 1 == FORWARD
-
-        if (Input.GetButtonDown("Jump") && jumpCounter < jumpTimes)
-        {
-            Jump();
-        }
+        // Handle flipping the character
+        if ((transform.lossyScale.x > 0 && horizontalMovement < 0) || transform.lossyScale.x < 0 && horizontalMovement > 0) { Flip(); }
     }
-    void FixedUpdate()//MOVE THE CHARECTER
+    void FixedUpdate()
     {
-        if ( jumpCounter < jumpTimes)
-        {
-            rigidBody.velocity = new Vector2(horizontalMove, rigidBody.velocity.y);
-
-        }
+        rigidBody.velocity = new Vector2(horizontalMovement * horizontalMovementSpeed * Time.deltaTime, rigidBody.velocity.y);
     }
 
-    void Jump()
-    {
-        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
-        jumpCounter++;
-    }
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground")) 
+        if (IsGrounded()) { jumpCounter = 0; }
+        Debug.Log(IsGrounded());
+    }
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
         {
-            jumpCounter = 0;
-        }
-        else if (Time.time < jumpCoolDown)
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
+            if (jumpCounter < jumpTimes)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+                Debug.Log(context.duration);
+                jumpCounter++;
+            }
         }
     }
-
-
-
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.canceled == false)
+        {
+            horizontalMovement = context.ReadValue<Vector2>().x;
+        } 
+        else
+        {
+            horizontalMovement = 0;
+        }
+    }
+    private void Flip()
+    {
+        var scale = transform.lossyScale;
+        transform.localScale = new Vector3(-scale.x, scale.y, scale.z);
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, groundedBoxCastOffset);
+    }
 }
